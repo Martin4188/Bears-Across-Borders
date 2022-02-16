a55b92272518fe3c0c1cbcf9f2827bc04ca585de
Martin4188 <martinandersson4188@gmail.com>
2022-02-16 15:40:04 +0100

    First draft of structure. Illustrating Problems.

--- Bears Script.R
+++ Bears Script.R
@@ -1,4 +1,19 @@
 library(tidyverse)
+library(sf)
 
-captures <- read.csv("data/captures.csv")
+captures <- read_csv("data/captures.csv")
+
+capturesFinal <- captures %>%
+  filter(sex == "Hona") %>%
+  group_by(id) %>%
+  mutate(meanlon = mean(lon), meanlat = mean(lat)) %>%
+  group_by(id) %>%
+  mutate(DistanceFromMean = sqrt((lon - meanlon)^2+(lat - meanlat)^2))
+
+map <- st_read("LanSweref99TM/Lan_Sweref99TM_region.shp", quiet = TRUE) %>% 
+  mutate(Inventering = case_when(LnKod == 25 ~ "2021",
+                                 LnKod %in% 22:23 ~ "2020",
+                                 LnKod == 24 ~ "2019",
+                                 LnKod %in% 20:21 ~ "2017"))
+
 
